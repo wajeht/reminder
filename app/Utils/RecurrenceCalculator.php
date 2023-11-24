a1bdb65c8407461d31cd6e9458a41253366a157f
wajeht <58354193+wajeht@users.noreply.github.com>
2023-11-23 22:44:56 -0600

    feat: RecurrenceCalculator

---
+++ app/Utils/RecurrenceCalculator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Utils;
+
+use App\Enums\RecurringType;
+use Carbon\Carbon;
+
+class RecurrenceCalculator
+{
+  public static function calculateNextOccurrence($recurrence, Carbon $startDate): Carbon
+  {
+    return match ($recurrence->type) {
+      RecurringType::Daily->value => $startDate->addDay(),
+      RecurringType::Weekly->value => $startDate->addWeek(),
+      RecurringType::BiWeekly->value => $startDate->addWeeks(2),
+      RecurringType::Monthly->value => $startDate->addMonth(),
+      RecurringType::Yearly->value => $startDate->addYear(),
+      RecurringType::Custom->value => $startDate->addDays($recurrence->interval),
+      default => throw new \Exception("Invalid recurrence type")
+    };
+  }
+}
