d9b6994812795e15cd1d6197572cff2706672461
Jaw <58354193+wajeht@users.noreply.github.com>
2023-11-24 06:18:39 -0600

    feat: somem layout stuff

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -37,72 +37,79 @@ const submit = () => {
             {{ status }}
         </div>
 
-        <form @submit.prevent="submit" class="flex flex-col gap-6">
-            <h2 class="text-2xl font-semibold leading-tight text-gray-800 dark:text-gray-200 text-center">
+        <form
+            @submit.prevent="submit"
+            class="flex flex-col gap-6">
+            <h2
+                class="text-2xl font-semibold leading-tight text-gray-800 dark:text-gray-200 text-center">
                 Login
             </h2>
 
             <div>
-                <InputLabel
-                    for="email"
-                    value="Email" />
+                <div>
+                    <InputLabel
+                        for="email"
+                        value="Email" />
 
-                <TextInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                    autocomplete="username" />
+                    <TextInput
+                        id="email"
+                        v-model="form.email"
+                        type="email"
+                        class="mt-1 block w-full"
+                        required
+                        autofocus
+                        autocomplete="username" />
 
-                <InputError
-                    class="mt-2"
-                    :message="form.errors.email" />
-            </div>
+                    <InputError
+                        class="mt-2"
+                        :message="form.errors.email" />
+                </div>
 
-            <div class="mt-4">
-                <InputLabel
-                    for="password"
-                    value="Password" />
+                <div class="mt-4">
+                    <InputLabel
+                        for="password"
+                        value="Password" />
 
-                <TextInput
-                    id="password"
-                    v-model="form.password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="current-password" />
+                    <TextInput
+                        id="password"
+                        v-model="form.password"
+                        type="password"
+                        class="mt-1 block w-full"
+                        required
+                        autocomplete="current-password" />
 
-                <InputError
-                    class="mt-2"
-                    :message="form.errors.password" />
-            </div>
+                    <InputError
+                        class="mt-2"
+                        :message="form.errors.password" />
+                </div>
 
-            <div class="mt-4 block">
-                <label class="flex items-center">
-                    <Checkbox
-                        v-model:checked="form.remember"
-                        name="remember" />
+                <div class="mt-4 block">
+                    <label class="flex items-center">
+                        <Checkbox
+                            v-model:checked="form.remember"
+                            name="remember" />
 
-                    <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
-                </label>
-            </div>
+                        <span class="ms-2 text-sm text-gray-600 dark:text-gray-400"
+                            >Remember me</span
+                        >
+                    </label>
+                </div>
 
-            <div class="mt-4 flex items-center justify-end">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800">
-                    Forgot your password?
-                </Link>
+                <div class="mt-4 flex items-center justify-end">
+                    <Link
+                        v-if="canResetPassword"
+                        :href="route('password.request')"
+                        class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800">
+                        Forgot your password?
+                    </Link>
 
-                <PrimaryButton
-                    class="ms-4"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
+                    <PrimaryButton
+                        class="ms-4"
+                        :class="{ 'opacity-25': form.processing }"
+                        :disabled="form.processing">
+                        Log in
+                    </PrimaryButton>
+                </div>
             </div>
         </form>
     </GuestLayout>
