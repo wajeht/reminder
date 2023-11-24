85703d1ef1832ff20725942593ce47c0ab664f82
wajeht <58354193+wajeht@users.noreply.github.com>
2023-11-23 22:43:59 -0600

    feat: more recurrence types

--- app/Enums/RecurringType.php
+++ app/Enums/RecurringType.php
@@ -6,6 +6,13 @@ enum RecurringType: string
 {
   case Daily = 'daily';
   case Weekly = 'weekly';
+  case BiWeekly = 'bi-weekly';
   case Monthly = 'monthly';
+  case Yearly = 'yearly';
   case Custom = 'custom';
+
+  public static function getValues(): array
+  {
+    return array_column(self::cases(), 'value');
+  }
 }
