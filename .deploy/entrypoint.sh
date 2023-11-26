502193820a791116d3e13ad90cd2dde2c3f4bfe9
Jaw <58354193+wajeht@users.noreply.github.com>
2023-11-26 02:37:53 -0600

    feat: force create admin user auto on prod

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -11,7 +11,7 @@ touch database/database.sqlite
 npm run build:prod
 npm run ssr
 php artisan migrate --no-interaction --force
-php artisan db:seed --class=AdminUserSeeder
+php artisan db:seed --class=AdminUserSeeder --no-interaction --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
