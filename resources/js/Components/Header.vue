d1f3ebc30c72ada085528370ae4423477a61c8be
Jaw <58354193+wajeht@users.noreply.github.com>
2023-11-25 18:55:52 -0600

    chore: husky

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -30,7 +30,9 @@ import NavLink from '@/Components/NavLink.vue';
                     class="inline-flex items-center justify-center gap-2 border-b-2 border-transparent px-1 pt-1 text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out hover:border-gray-300 hover:text-gray-700 focus:border-gray-300 focus:text-gray-700 focus:outline-none dark:text-gray-400 dark:hover:border-gray-700 dark:hover:text-gray-300 dark:focus:border-gray-700 dark:focus:text-gray-300"
                     href="https://www.github.com/wajeht/reminder"
                     target="_blank">
-                    <Icon icon="bi:github" class="text-xs" />
+                    <Icon
+                        icon="bi:github"
+                        class="text-xs" />
                     Github
                 </a>
 
