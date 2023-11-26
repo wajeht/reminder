6405ff20e0283ef20096d69a0d70cc67749de1b1
Jaw <58354193+wajeht@users.noreply.github.com>
2023-11-25 18:45:36 -0600

    chore: primevue

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -27,7 +27,7 @@ import NavLink from '@/Components/NavLink.vue';
 
             <div class="flex items-center justify-center gap-4">
                 <a
-                    class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out hover:border-gray-300 hover:text-gray-700 focus:border-gray-300 focus:text-gray-700 focus:outline-none dark:text-gray-400 dark:hover:border-gray-700 dark:hover:text-gray-300 dark:focus:border-gray-700 dark:focus:text-gray-300 gap-2 justify-center"
+                    class="inline-flex items-center justify-center gap-2 border-b-2 border-transparent px-1 pt-1 text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out hover:border-gray-300 hover:text-gray-700 focus:border-gray-300 focus:text-gray-700 focus:outline-none dark:text-gray-400 dark:hover:border-gray-700 dark:hover:text-gray-300 dark:focus:border-gray-700 dark:focus:text-gray-300"
                     href="https://www.github.com/wajeht/reminder"
                     target="_blank">
                     <Icon icon="bi:github" class="text-xs" />
--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,10 +1,13 @@
 import './bootstrap';
 import '../css/app.css';
+import 'primevue/resources/themes/lara-light-green/theme.css';
 
 import { createApp, h, DefineComponent } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import ToastService from 'primevue/toastservice';
+import PrimeVue from 'primevue/config';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -20,6 +23,8 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(ToastService)
+            .use(PrimeVue, { pt: {} })
             .mount(el);
     },
     progress: {
