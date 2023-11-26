b088d9856e167a365f7a7b45aae974af15fddbc5
Jaw <58354193+wajeht@users.noreply.github.com>
2023-11-26 07:53:27 -0600

    feat: sample list view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
