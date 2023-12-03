9223e625b45c4ec81236801e13bd011ba82a2384
Jaw <58354193+wajeht@users.noreply.github.com>
2023-12-03 02:21:48 -0600

    feat: wip

--- app/Http/Controllers/Api/V1/EventController.php
+++ app/Http/Controllers/Api/V1/EventController.php
@@ -32,6 +32,25 @@ class EventController extends Controller
         ]);
     }
 
+    public function create(Request $request): JsonResponse
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'description' => 'nullable|string',
+            'start_date' => 'required|date',
+            'end_date' => 'nullable|date|before_or_equal:start_date'
+        ]);
+
+        $event = Event::create([
+            ...$request->all(),
+        ]);
+
+        return response()->json([
+            'message' => 'ok',
+            'data' => [$event],
+        ]);
+    }
+
     public function destroy(Request $request, Event $event): JsonResponse
     {
         $this->authorize('delete', $event);
